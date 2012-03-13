eec49bfab1199a0a71233d3d8ec44c10a8326b4e
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2012-03-13 13:59:19 +0000

    Initial Commit

---
+++ VideoStreamer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoStreamer
+//
+//  Created by Joseph caxton-Idowu on 13/03/2012.
+//  Copyright (c) 2012 caxtonidowu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ VideoStreamer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VideoStreamer
+//
+//  Created by Joseph caxton-Idowu on 13/03/2012.
+//  Copyright (c) 2012 caxtonidowu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
