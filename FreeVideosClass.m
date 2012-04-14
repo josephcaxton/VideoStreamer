5a895b6275b2bd355b2e699b42e54b9873fc21a7
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2012-04-14 12:04:20 +0100

    right size img

--- FreeVideosClass.m
+++ FreeVideosClass.m
@@ -32,8 +32,8 @@
     int i;
     NSString *loadString;
     
-    for(i = 0; i < 55; i++) {
-        loadString = [NSString stringWithFormat:@"img%d", i]; 
+    for(i = 0; i < 58; i++) {
+        loadString = [NSString stringWithFormat:@"img%d", i + 1]; 
         [ImageObjects addObject:[UIImage imageNamed:loadString]];
         
     }
