48a01d07e8522e0dcf0d6661019cf2970bbe507b
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2012-04-16 13:19:32 +0100

    imgs

--- FreeVideosClass.m
+++ FreeVideosClass.m
@@ -32,7 +32,7 @@
     int i;
     NSString *loadString;
     
-    for(i = 0; i < 58; i++) {
+    for(i = 0; i < 70; i++) {
         loadString = [NSString stringWithFormat:@"img%d", i + 1]; 
         [ImageObjects addObject:[UIImage imageNamed:loadString]];
         
